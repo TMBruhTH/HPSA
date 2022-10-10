db7cb0550462ce82dcdd98fb13c99085c8132bda
TMBruhTH <brunno_1211@hotmail.com>
2022-10-10 17:04:46 -0300

    ..

---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+//teste hahahahahaha
+//teste hahahahahaha
+//teste hahahahahaha
+//teste hahahahahaha
+//teste hahahahahaha
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Frontend';
+}
