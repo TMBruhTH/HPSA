793e072697dcbe6b1fcb4bd6f608ef0b143d0a76
TMBruhTH <brunno_1211@hotmail.com>
2022-10-14 09:37:11 -0300

    inclusão de user register

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -11,13 +11,17 @@ import { PropertyListComponent } from './property/property-list/property-list.co
 import { HousingService } from './services/housing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
 
 const appRoutes: Routes = [
   {path: '', component: PropertyListComponent},
   {path: 'rent-property', component: PropertyListComponent},
   {path: 'add-property', component: AddPropertyComponent},
   {path: 'property-detail/:id', component: PropertyDetailComponent},
+  {path: 'user/login', component: UserLoginComponent},
+  {path: 'user/register', component: UserRegisterComponent},
   {path: '**', component: PropertyListComponent}
 ];
 
@@ -29,13 +33,16 @@ const appRoutes: Routes = [
     PropertyListComponent,
     AddPropertyComponent,
     PropertyDetailComponent,
+    UserLoginComponent,
+    UserRegisterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes),
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [HousingService],
   bootstrap: [AppComponent],
