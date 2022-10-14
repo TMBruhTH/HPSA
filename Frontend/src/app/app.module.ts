54a969c5cfe5ffc72d7084e306d592f95428c7ba
TMBruhTH <brunno_1211@hotmail.com>
2022-10-14 12:02:21 -0300

    interface User config. Array

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PropertyDetailComponent } from './property/property-detail/property-det
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UserLoginComponent } from './user/user-login/user-login.component';
 import { UserRegisterComponent } from './user/user-register/user-register.component';
+import { UserServiceService } from './services/user-service.service';
 
 const appRoutes: Routes = [
   {path: '', component: PropertyListComponent},
@@ -44,7 +45,7 @@ const appRoutes: Routes = [
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [HousingService],
+  providers: [HousingService, UserServiceService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ Frontend/src/app/services/user-service.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { User } from '../model/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserServiceService {
+  constructor() {}
+
+  addUser(user: User) {
+    let users = new Array();
+
+    if (localStorage.getItem('Users')) {
+      var sUsers: string = '';
+
+      sUsers = JSON.parse(localStorage.getItem('Users') as string);
+      users.push(sUsers);
+    }
+
+    users.push(user);
+
+    localStorage.setItem('Users', JSON.stringify(users));
+  }
+}
